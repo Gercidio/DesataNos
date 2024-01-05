e992e79dc38a0b6f16c3f2dd00e1c061aeaca07a
Gercidio <gercidio@gmail.com>
2024-01-05 10:33:57 -0300

    crud & corrections on pom

---
+++ src/main/java/com/desatanos/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package com.desatanos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.desatanos.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo") String tipo);
+	
+	List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+
+}
